0666c5f888fb8d648f74a2b8dbd881c391d59dab
Romain MARCA <romain.marca@sii.fr>
2019-05-15 16:56:29 +0200

    first commit

---
+++ README.md
@@ -0,0 +1,3 @@
+### Lesson OpenClassRoom for DOM concept
+
+https://openclassrooms.com/fr/courses/3306901-creez-des-pages-web-interactives-avec-javascript/3501871-decouvrez-le-dom
---
+++ chapitre_2/js/reve.js
@@ -0,0 +1,10 @@
+// TODO : écrire la fonction compterElements
+
+function compterElements(elmt)  {
+   return document.querySelectorAll(elmt).length;
+}
+
+console.log(compterElements("p")); // Doit afficher 4
+console.log(compterElements(".adjectif")); // Doit afficher 3
+console.log(compterElements("p .adjectif")); // Doit afficher 3
+console.log(compterElements("p > .adjectif")); // Doit afficher 2
